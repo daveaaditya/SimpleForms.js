24a527686715dbbfd24b76f07e23a8a14c1f91b1
Aaditya Dave <anb.dave@gmail.com>
2020-07-22 14:35:27 -0400

    Added examples.js

---
+++ simple-forms.js
@@ -0,0 +1,35 @@
+class Survey {
+  constructor() {
+    this.surveyName = '';
+    this.survey 
+  }
+}
+
+class InputBox {
+  constructor() {
+    this.label = '';
+    this.value = '';
+    this.id = '';
+  }
+}
+
+function createForm() {
+  const form = document.createElement('form');
+  console.log(form);
+  return form;
+}
+
+// This id determines where the form will be placed.
+function addFormToPage(id) {
+
+}
+
+// const formsOnPage = document.querySelectorAll('.simple-form');
+console.log(formsOnPage);
+
+
+// Input
+// Dropdown
+// Checkbox
+// Submit
+
