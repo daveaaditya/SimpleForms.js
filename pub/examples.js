d2ca5fc249cf326afaed222e4ac2efabb2c6e26c
Aaditya Dave <anb.dave@gmail.com>
2020-07-26 11:05:58 -0400

    Added basic colouring options.

--- pub/examples.js
+++ pub/examples.js
@@ -15,6 +15,15 @@ MC.addCheckbox('3', 'right');
 MC.addCheckbox('5', 'right');
 MC.addSubmitButton('Check answer', function(e) {
   e.preventDefault();
-  console.log(MC.getFormData());
-})
+  const MCSubmission = MC.getFormData();
+
+  if (MCSubmission[0] === false &&
+      MCSubmission[1] === true &&
+      MCSubmission[2] === false &&
+      MCSubmission[3] === false) {
+    MC.addStyle('green-bd');
+  } else {
+    MC.addStyle('red-bd');
+  }
+});
 MC.buildForm('multiple-choice');
